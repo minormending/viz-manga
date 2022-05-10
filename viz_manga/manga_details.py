389428997942a9bbb927a85dfd3aed7d9b783e2f
minormending <54426030+minormending@users.noreply.github.com>
2022-05-10 09:59:39 -0400

    fight with bs4 to get sub class find to work

--- viz_manga/manga_details.py
+++ viz_manga/manga_details.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+import re
+from bs4 import BeautifulSoup, ResultSet, Tag
 from requests import Response, Session
 
 
@@ -14,3 +15,13 @@ class VizMangaDetails:
         resp: Response = self.session.get(url)
 
         soup: BeautifulSoup = BeautifulSoup(resp.text, "html.parser")
+        series_section: Tag = soup.find(class_="section_chapters")
+        series_table: Tag = series_section.find("div", {"class":"property-row"})
+        series_tags: ResultSet = series_table.find_all(href=re.compile("/shonenjump/chapters/"), class_="o_chapters-link")
+        for series_tag in series_tags[0:3]:
+            print(series_tag)
+
+
+if __name__ == "__main__":
+    details = VizMangaDetails()
+    details.get_series()
