a2835ecac2030c80234c6f1ebc531ad36713feb0
minormending <54426030+minormending@users.noreply.github.com>
2022-05-10 08:45:20 -0400

    start looking up series

--- viz_manga/manga_details.py
+++ viz_manga/manga_details.py
@@ -8,3 +8,9 @@ class VizMangaDetails:
         self.session.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
         }
+
+    def get_series(self) -> None:
+        url: str = "https://www.viz.com/shonenjump"
+        resp: Response = self.session.get(url)
+
+        soup: BeautifulSoup = BeautifulSoup(resp.text, "html.parser")
