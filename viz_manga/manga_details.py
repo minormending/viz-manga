a0bf550f0c7352a0e939e17b7bddd42be1c1dbfb
minormending <54426030+minormending@users.noreply.github.com>
2022-05-10 08:41:45 -0400

    outline for details

---
+++ viz_manga/manga_details.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+from requests import Session
+
+
+class VizMangaDetails:
+    def __init__(self) -> None:
+        self.session = Session()
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
+        }
