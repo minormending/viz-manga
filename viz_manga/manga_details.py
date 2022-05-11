ec1e225126a6c04a9cdf051bd087e4cc969b2704
minormending <54426030+minormending@users.noreply.github.com>
2022-05-11 12:35:52 -0400

    get chapter ids

--- viz_manga/manga_details.py
+++ viz_manga/manga_details.py
@@ -14,6 +14,14 @@ class Series:
         return f"https://www.viz.com/shonenjump/chapters/{self.slug}"
 
 
+@dataclass
+class Chapter:
+    title: str
+    id: str
+    link: str
+    is_free: bool = False
+
+
 class VizMangaDetails:
     def __init__(self) -> None:
         self.session = Session()
@@ -36,13 +44,61 @@ class VizMangaDetails:
         for series_tag in series_tags:
             link: str = series_tag["href"]
             slug: str = link.replace(series_link_pattern, "").strip()
-            name = series_tag.find_all("div", class_="type-center")[-1].text.strip()
-            
+            name: str = series_tag.find_all("div", class_="type-center")[
+                -1
+            ].text.strip()
+
             series.append(Series(name, slug))
         return sorted(series, key=lambda s: s.name.lower())
 
+    def get_series_chapters(self, series: Series) -> List[Chapter]:
+        resp: Response = self.session.get(series.link)
+
+        chapter_link_pattern: str = "/shonenjump/[^/]+/chapter/(\d+)"
+
+        soup: BeautifulSoup = BeautifulSoup(resp.text, "html.parser")
+        chapter_tags: ResultSet = soup.find_all(
+            lambda tag: tag.name == "a"
+            and "ch-" in tag.get("id", "")
+            and re.search(chapter_link_pattern, tag.get("data-target-url", ""))
+        )
+
+        chapters: List[Chapter] = []
+        for chapter_tag in chapter_tags:
+            link: str = chapter_tag["data-target-url"]
+            match: re.Match = re.search(chapter_link_pattern, link)
+            chapter_id: str = match.group(1)
+            link = f"https://www.viz.com{match.group(0)}"
+
+            name: str = chapter_tag["id"]
+            is_free: bool = "join to read" not in chapter_tag.get("href", "").lower()
+            chapters.append(Chapter(name, chapter_id, link, is_free))
+        return chapters
+
+
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Lookup Viz managa information.")
+    subparsers = parser.add_subparsers(dest="command")
+    series_parser = subparsers.add_parser(
+        "series",
+        help="Get series title and slug (for chapter lookup) obtained from the Viz site.",
+    )
+    chapter_parser = subparsers.add_parser(
+        "chapters", help="Get chapter title and id obtained from the Viz site."
+    )
+    chapter_parser.add_argument(
+        "series_slug",
+        help="Series title for which to lookup chapter ids from the Viz site.",
+    )
+
+    args = parser.parse_args()
+
     details = VizMangaDetails()
-    series: List[Series] = details.get_series()
-    for manga in series:
-        print(f"{manga.name} : {manga.slug}")
+    if args.command == "series":
+        series: List[Series] = details.get_series()
+        for manga in series:
+            print(manga.__dict__)
+    elif args.command == "chapters" and args.series_slug:
+        for chapter in details.get_series_chapters(Series(None, args.series_slug)):
